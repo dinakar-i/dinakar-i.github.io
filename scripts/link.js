2eefd303cd2e0b1348b3e11890c4963c5ae06137
Dinakar <115007967+dinakar-i@users.noreply.github.com>
2023-05-27 12:19:10 +0530

    Fourth commit

--- scripts/link.js
+++ scripts/link.js
@@ -1,16 +1,29 @@
-$('.fa-square-github').click(()=>
-{
-	window.open("https://github.com/dinakar-i");
+$('.fa-square-github').click(() => {
+    window.open("https://github.com/dinakar-i");
 })
-$('.fa-linkedin').click(()=>
-{
-	window.open("https://www.linkedin.com/in/dinakar-m-3b56531b9/");
+$('.fa-linkedin').click(() => {
+    window.open("https://www.linkedin.com/in/dinakar-m-3b56531b9/");
 })
-$('.fa-instagram').click(()=>
-{
-	window.open("https://www.instagram.com/dinakar_i/");
+$('.fa-instagram').click(() => {
+    window.open("https://www.instagram.com/dinakar_i/");
 })
-$('.fa-envelope').click(()=>
-{
-	window.open("mailto:dinakar_i@yahoo.com");
+$('.fa-envelope').click(() => {
+    window.open("mailto:dinakar_i@yahoo.com");
 })
+
+// $(".aboutme-btn").click(function() {	
+//     $('html,body').animate({
+//             scrollTop: $(".aboutme-p").offset().top
+//         },
+//         'slow');
+// });
+
+$(document).ready(function() {
+    $(".aboutme-btn").click(function(event) {
+        console.log("trigger");
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $(".aboutme-p").offset().top
+        }, 500);
+    });
+});
